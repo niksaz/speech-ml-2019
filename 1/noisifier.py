53916268048ecf3692ef675d70656fd6c7667ca8
niksaz <nikitasazanovich@gmail.com>
2019-02-05 14:33:58 +0300

    Switched to pathlib.

--- 1/noisifier.py
+++ 1/noisifier.py
@@ -1,22 +1,25 @@
 # Author: Mikita Sazanovich
 
-import os
+from pathlib import Path
 import librosa
 import random
 import numpy as np
 import argparse
 
+BEEP_SOUND_AMPL_COEF = 0.25
+BACK_SOUND_AMPL_COEF = 0.25
+
 
 def load_audio_sec(file_path, sr=16000):
-    data, _ = librosa.core.load(file_path, sr)
+    data, _ = librosa.core.load(str(file_path), sr)
     return data
 
 
-def get_random_file_in(directory):
-    assert os.path.isdir(directory)
-    files = os.listdir(directory)
-    file_name = files[random.randrange(len(files))]
-    return os.path.join(directory, file_name)
+def get_random_filepath_in(directory):
+    assert directory.is_dir()
+    files = list(directory.iterdir())
+    filepath = files[random.randrange(len(files))]
+    return filepath
 
 
 def repeat_or_trim_to(data, wanted_length):
@@ -27,10 +30,10 @@ def repeat_or_trim_to(data, wanted_length):
         return np.pad(data, (0, wanted_length - N), 'wrap')
 
 
-def augment_speech(speech_file, output_file, beep_file, back_file, sr):
-    speech_data = load_audio_sec(speech_file, sr=sr)
-    beep_data = load_audio_sec(beep_file, sr=sr)
-    back_data = load_audio_sec(back_file, sr=sr)
+def augment_speech(speech_path, output_path, beep_path, back_path, sr):
+    speech_data = load_audio_sec(speech_path, sr=sr)
+    beep_data = load_audio_sec(beep_path, sr=sr)
+    back_data = load_audio_sec(back_path, sr=sr)
 
     N = len(speech_data)
     beep_data = repeat_or_trim_to(beep_data, N)
@@ -38,21 +41,19 @@ def augment_speech(speech_file, output_file, beep_file, back_file, sr):
 
     assert N == len(beep_data) and N == len(back_data)
 
-    output = speech_data + 0.25 * beep_data + 0.25 * back_data
-    librosa.output.write_wav(output_file, output, sr=sr)
+    output = (speech_data
+              + BEEP_SOUND_AMPL_COEF * beep_data
+              + BACK_SOUND_AMPL_COEF * back_data)
+    librosa.output.write_wav(str(output_path), output, sr=sr)
 
 
 def augment_speech_files(speech_dir, output_dir, beep_dir, back_dir):
-    if not os.path.exists(output_dir):
-        os.makedirs(output_dir)
-
-    for speech_filename in os.listdir(speech_dir):
-        speech_file = os.path.join(speech_dir, speech_filename)
-        speech_base = os.path.splitext(speech_filename)[0]
-        output_file = os.path.join(output_dir, speech_base + '-aug.wav')
-        beep_file = get_random_file_in(beep_dir)
-        back_file = get_random_file_in(back_dir)
-        augment_speech(speech_file, output_file, beep_file, back_file, sr=16000)
+    output_dir.mkdir(exist_ok=True)
+    for speech_path in speech_dir.iterdir():
+        output_path = output_dir / f'{speech_path.stem}-aug.wav'
+        beep_path = get_random_filepath_in(beep_dir)
+        back_path = get_random_filepath_in(back_dir)
+        augment_speech(speech_path, output_path, beep_path, back_path, sr=16000)
 
 
 def main():
@@ -69,7 +70,7 @@ def main():
     args = parser.parse_args()
 
     augment_speech_files(
-        args.speech_dir, args.output_dir, args.beep_dir, args.back_dir)
+        Path(args.speech_dir), Path(args.output_dir), Path(args.beep_dir), Path(args.back_dir))
 
 
 if __name__ == '__main__':
